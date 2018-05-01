c7966fd626f119703e9bf90d586959f71c41b31e
Sean <18744415+SeanMcCord@users.noreply.github.com>
2018-05-01 00:01:18 -0400

    Revert "redirect all www to non www, there may be a better way to do this"
    This reverts commit e922b2964af8bf779622e23e5acc7d5d19c1e812.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  constraints(host: /^www\./i) do
-    match '(*any)' => redirect { |params, request|
-      URI.parse(request.url).tap { |uri| uri.host.sub!(/^www\./i, '') }.to_s
-    }
-  end
-
   root to: 'overview#index'
 end
