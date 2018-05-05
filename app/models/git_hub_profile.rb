6673e53b16d8dab2b8c5a6a91e42a8129e2c9d66
Sean <18744415+SeanMcCord@users.noreply.github.com>
2018-05-05 02:30:45 -0400

    add git hub profile module

---
+++ app/models/git_hub_profile.rb
@@ -0,0 +1,7 @@
+module GitHubProfile
+  class << self
+    def sean_mccord_url
+      'https://github.com/SeanMcCord'
+    end
+  end
+end
---
+++ spec/models/git_hub_profile_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe GitHubProfile do
+  it 'defines a class method for my github profile' do
+    url = GitHubProfile.sean_mccord_url
+    # TODO consider having this validate the url is a valid url
+    expect(url).to include('github', 'McCord')
+  end
+end
