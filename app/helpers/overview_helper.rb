e93737fe2f592c170cdc0622da7831f0738fcc41
Sean <18744415+SeanMcCord@users.noreply.github.com>
2018-05-16 02:00:05 -0400

    add sean description

--- app/helpers/overview_helper.rb
+++ app/helpers/overview_helper.rb
@@ -1,2 +1,13 @@
 module OverviewHelper
+  def sean_hero_description
+%Q(<br><br><span class="klass">#&lt;Human:0x#{SecureRandom.hex(10)} </span>
+ @long_name=<span class="string">"Sean McCord"</span>
+ @job=
+  <span class="klass">#&lt;Job:0x#{SecureRandom.hex(10)} </span>
+   @title=<span class="string">"Developer"</span>
+   @company=
+    <span class="klass">#&lt;Company:0x#{SecureRandom.hex(10)} </span>
+     @name=<span class="string">"Amazon"</span>
+ @likes=[<span class="symbol">:plants</span>, <span class="symbol">:food</span>])
+  end
 end
